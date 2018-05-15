901d902976b72c8b6a44da7f2e39ec748b7d8d50
Tim Heighes <tim@heighes.com>
2018-05-15 21:00:08 +0200

    Refactor css and add a webfont
    yarn eject, yarn add css-modules postcss-nested normalize.css webfontloader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import css from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={css.App}>
+        <header className={css.AppHeader}>
+          <img src={logo} className={css.AppLogo} alt="logo" />
+          <h1 className={css.AppTitle}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={css.AppIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
